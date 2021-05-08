c84ce979f58de1befba3a11299cbcfc61fe9720a
nicostranieri21 <72672764+nicostranieri21@users.noreply.github.com>
2021-05-08 19:22:33 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from shapely.geometry import LineString
+from shapely.geometry import Point
+
+A = Point(0,0)
+c = A.buffer(1).boundary
+l = LineString([(-2,2), (2, -2)])
+i = c.intersection(l)
+print(i)
+B = None
+if i.type == "Point":
+    B = i
+else:
+    for point in i:
+        if B is None:
+            B = point
+            break
+print(B)
+
