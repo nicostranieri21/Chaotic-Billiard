a1ed9ee7729ead8b38a07554dc8013ba96c67efd
ismabo <75274379+ismabo@users.noreply.github.com>
2021-05-10 14:15:29 +0000

    Create regular_billiard_lines.py

---
+++ regular_billiard_lines.py
@@ -0,0 +1,184 @@
+from tkinter import *
+import time
+import math
+
+
+#Counter clockwise rotational matrix
+def counterclockwise(vx,vy,theta):
+    alfa=math.pi-2*theta
+    dx=vx*math.cos(alfa)+vy*math.sin(alfa)
+    dy=(-1)*vx*math.sin(alfa)+vy*math.cos(alfa)
+    l=[dx,dy]
+    return l
+#Clockwise rotational matrix
+def clockwise(vx,vy,theta):
+    alfa=math.pi-2*theta
+    dx=vx*math.cos(alfa)-vy*math.sin(alfa)
+    dy=vx*math.sin(alfa)+vy*math.cos(alfa)
+    l=[dx,dy]
+    return l
+#Function which determines which angle particle should 
+#bounce at(returns new x and y components of the velocity vector)
+def collide(x,y,vx,vy,x0,y0):
+    nx=x-x0
+    ny=y-y0
+    dnx=nx/((nx**2+ny**2)**0.5)
+    dny=ny/((nx**2+ny**2)**0.5)
+    dvx=vx/((vx**2+vy**2)**0.5)
+    dvy=vy/((vx**2+vy**2)**0.5)
+    theta=math.acos((dnx*dvx+dny*dvy)/(((dvx**2+dvy**2)**0.5)*((dnx**2+dny**2)**0.5)))  
+    
+    if(x-x0>=0):   
+        if(y-y0>=0):
+            npp=+1
+            if(dvx<=0 and dvy<=0):
+                if(abs(dvx)<=dnx):
+                    
+                    vel=counterclockwise(vx,vy,theta)
+                    vx=vel[0]
+                    vy=vel[1]
+                else:
+                    vel=clockwise(vx,vy,theta)
+                    vx=vel[0]
+                    vy=vel[1]
+            elif(dvx<=0 and dvy>=0):
+                vel=clockwise(vx,vy,theta)
+                vx=vel[0]
+                vy=vel[1]
+            else:
+                vel=counterclockwise(vx,vy,theta)
+                vx=vel[0]
+                vy=vel[1]
+        else:
+            if(dvx<=0 and dvy>=0):
+                if(abs(dvx)<=(dnx)):
+                    vel=clockwise(vx,vy,theta)
+                    vx=vel[0]
+                    vy=vel[1]
+                else:
+                    vel=counterclockwise(vx,vy,theta)
+                    vx=vel[0]
+                    vy=vel[1]
+            elif(dvx<=0 and dvy<=0):
+                vel=counterclockwise(vx,vy,theta)
+                vx=vel[0]
+                vy=vel[1]
+            else:
+                vel=clockwise(vx,vy,theta)
+                vx=vel[0]
+                vy=vel[1]
+    else:       
+        if(y-y0<=0):
+            if(dvx>=0 and dvy>=0):
+                if(dvx<=abs(dnx)):
+                    vel=counterclockwise(vx,vy,theta)
+                    vx=vel[0]
+                    vy=vel[1]
+                else:
+                    vel=clockwise(vx,vy,theta)
+                    vx=vel[0]
+                    vy=vel[1]
+            elif(dvx>=0 and dvy <0):
+                vel=clockwise(vx,vy,theta)
+                vx=vel[0]
+                vy=vel[1]
+            else:
+                vel=counterclockwise(vx,vy,theta)
+                vx=vel[0]
+                vy=vel[1]
+        else:
+            if(dvx>=0 and dvy<=0):
+                if(abs(dvx)>=abs(dnx)):
+                    vel=counterclockwise(vx,vy,theta)
+                    vx=vel[0]
+                    vy=vel[1]
+                else:
+                    vel=clockwise(vx,vy,theta)
+                    vx=vel[0]
+                    vy=vel[1]
+            elif(dvx>0 and dvy>=0):
+                vel=counterclockwise(vx,vy,theta)              
+                vx=vel[0]
+                vy=vel[1]
+            else:
+                vel=clockwise(vx,vy,theta)
+                vx=vel[0]
+                vy=vel[1]               
+          
+    return vx, vy
+            
+class Particle:
+    def __init__(self,canvas,x,y,diameter,xVelocity,yVelocity,color,obstacles):
+        self.canvas = canvas
+        self.image = canvas.create_oval(x-diameter,y-diameter,diameter+x,diameter+y,fill=color)
+        self.xVelocity = xVelocity
+        self.yVelocity = yVelocity
+        self.x=x
+        self.y=y
+        self.color=color
+        #obstacle parameters(used for detecting collision) 
+        self.radius=obstacles.radius
+        self.x0=obstacles.x
+        self.y0=obstacles.y
+        self.rectBord=obstacles.rectangleBorders
+
+
+    def move(self, dt):
+        coordinates = self.canvas.coords(self.image)
+        self.x=coordinates[2]
+        self.y=coordinates[3]
+        canvas.create_line(self.x, self.y, self.x-self.xVelocity,
+                           self.y-self.yVelocity, fill=self.color, width=1)
+        #checks if particle collided with top, bottom, right or left boundary
+        #of billiard and calculates new velocity vector
+        if(coordinates[2]>=(self.rectBord[2]) or coordinates[0]<=self.rectBord[0]):
+            self.xVelocity = -self.xVelocity
+        if(coordinates[3]>=(self.rectBord[3]) or coordinates[1]<=self.rectBord[1]):
+            self.yVelocity = -self.yVelocity
+        
+
+        self.canvas.move(self.image,self.xVelocity,self.yVelocity)
+    
+class SinaiMap():
+    def __init__(self, canvas, x, y, diameter, billiard, color):
+        self.image = canvas.create_rectangle(billiard[0], billiard[1],
+                                             billiard[2], billiard[3])
+        self.x=x
+        self.y=y
+        self.radius=diameter
+        self.rectangleBorders=billiard
+    
+root = Tk()
+
+WIDTH = 800
+HEIGHT = 800
+
+canvas = Canvas(root,width=WIDTH,height=HEIGHT)
+canvas.pack()
+
+
+sinaiData={'centerX':370, 'centerY':285, 'circleRadius': 170, 'billiard':[30,30,770,600],
+           'billiardX0': 30, 'billiardY0':30,'billiardX1': 770, 'billiardY1': 400}
+
+obstacles=SinaiMap(canvas, sinaiData['centerX'],sinaiData['centerY'],sinaiData['circleRadius'],
+                   sinaiData['billiard'], "red")
+
+#this is creating set of particles
+particles=[]
+numOfParticles=2       #number of particles
+x=50                   #starting position for first particle
+y=50                   #rest will be generated approximately near the first particle
+particles.append(Particle(canvas, x, y, 3, -3, 3.1,"red",obstacles))
+particles.append(Particle(canvas, x +0.1, y +0.1, 3, -3, 3.1,"blue",obstacles))
+
+
+#this is where animation happens
+#dt determines the speed(lower=faster)
+dt=0.01
+while True:
+    for i in range(0, numOfParticles):
+        particles[i].move(dt)
+    root.update()
+    time.sleep(dt)
+
+root.mainloop()
